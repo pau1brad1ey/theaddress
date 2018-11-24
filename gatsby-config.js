b06e2c650fb5374127a404143d3e1c396fb0a086
pau1brad1ey <paul.bradley@greysphere.co.uk>
2018-11-24 22:57:18 +0000

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `The Address - Carcassonne, France`
   },
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-9907395-8',
+        head: true
+      }
+    }
+  ]
 }
